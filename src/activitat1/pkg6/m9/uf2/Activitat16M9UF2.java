ebbcc6c0b83deb97753e3a1ae14adaf5eb926c2b
davidblancolopez <daviddbl94@gmail.com>
2016-11-23 19:36:43 +0100

    Cambios.

--- src/activitat1/pkg6/m9/uf2/Activitat16M9UF2.java
+++ src/activitat1/pkg6/m9/uf2/Activitat16M9UF2.java
@@ -3,10 +3,22 @@ package activitat1.pkg6.m9.uf2;
 
 
 public class Activitat16M9UF2 {
-
+    private static Contenedor contenedor;
+    private static Thread productor;
+    private static Thread[] consumidores;
+    private static final int CANTIDADCONSUMIDORES = 5;
 
     public static void main(String[] args) {
-        
+    contenedor = new Contenedor();
+        productor = new Thread(new Productor(contenedor, 1));
+        consumidores = new Thread[CANTIDADCONSUMIDORES];
+
+        for (int i = 0; i < CANTIDADCONSUMIDORES; i++) {
+            consumidores[i] = new Thread(new Consumidor(contenedor, i));
+            consumidores[i].start();
+        }
+
+        productor.start();
     }
-    
+
 }
---
+++ src/activitat1/pkg6/m9/uf2/Contenedor.java
@@ -0,0 +1,35 @@
+
+package activitat1.pkg6.m9.uf2;
+
+
+public class Contenedor {
+
+    private int contenido;
+    private boolean contenedorLleno = Boolean.FALSE;
+
+    public synchronized int get() {
+        while (!contenedorLleno) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.err.println("Contenedor: Error en get -> " + e.getMessage());
+            }
+        }
+        contenedorLleno = !contenedorLleno;
+        notify();
+        return contenido;
+    }
+
+    public synchronized void put(int value) {
+        while (contenedorLleno) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.err.println("Contenedor: Error en put -> " + e.getMessage());
+            }
+        }
+        contenido = value;
+        contenedorLleno = !contenedorLleno;
+        notify();
+    }
+}
